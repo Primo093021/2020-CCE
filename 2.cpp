ccf6154eacf2cc4bf9cc34235bb3d67a5da5fcb3
Patty <32256079+Primo093021@users.noreply.github.com>
2021-05-06 19:03:37 +0800

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,8 +1,11 @@
+2. typedef 是 struct 的好朋友, 可以把 type 變簡單, typedef struct data {char c; int ans; } DATA; 可以用簡單的名字 DATA 取代很長的 struct data。請試著在 CodeBlocks 照著實作一下
+
+
 #include <stdio.h>
 typedef struct data
 {
-    char c;///���@�Ӧr��
-    int ans;///�X�{�X��
+    char c;///­þ¤@­Ó¦r¥À
+    int ans;///¥X²{´X¦¸
 }DATA;
 ///struct data listA;
 DATA listA;
