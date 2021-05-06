9c00e2b4f6c18571ad004f1ab4f07e5956d9354f
Patty <32256079+Primo093021@users.noreply.github.com>
2021-05-06 19:31:22 +0800

    Update 3.cpp

--- week11 正課內容/3.cpp
+++ week11 正課內容/3.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int compare(const void*p1,const void*p2)
-{///頧�(�湔��璅�)/���∴�皞�����
+{///轉成(整數的指標)/望遠鏡，準星看到的整數
     int d1=*( (int*)p1 );
     int d2=*( (int*)p2 );
     if(d1>d2) return 1;
