fb6296c9d68d8b518a3f7c14680e218189723b2f
Patty <32256079+Primo093021@users.noreply.github.com>
2021-05-06 19:03:02 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,4 +1,4 @@
-## 1. 老師用課本第7章第7-13頁介紹 typedef 前面是舊名字 新名字; 可以「再定義」, 請試著在 CodeBlocks 實作一下
+1. 老師用課本第7章第7-13頁介紹 typedef 前面是舊名字 新名字; 可以「再定義」, 請試著在 CodeBlocks 實作一下
 #include <stdio.h>
 unsigned char c;
 typedef unsigned char uchar;
